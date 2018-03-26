066f089db165cb3bc7baa945b32139dc69ebb403
Prischep <ya.pryshchep@yandex.by>
2018-03-27 00:38:43 +0300

    add task7

---
+++ task7/app.js
@@ -0,0 +1,24 @@
+let http = require("http");
+let fs = require("fs");
+
+http.createServer(function (request, response) {
+
+    console.log(`Запрошенный адрес: ${request.url}`);
+    if (request.url.startsWith('/public/')) {
+        let filePath = request.url.substr(1);
+        fs.readFile(filePath, function (error, data) {
+
+            if (error) {
+                response.statusCode = 404;
+                response.end('No resources found!!!');
+            }
+            else {
+                response.end(data);
+            }
+            return;
+        })
+    }
+    else {
+        response.end('Error!');
+    }
+}).listen(5000);
